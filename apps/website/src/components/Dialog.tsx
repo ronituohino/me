892091880e6733318965c9f65864d21219b11281
Roni Tuohino <rt@ronituohino.com>
2025-04-26 22:25:49 +0300

    Add dialog functionality

--- apps/website/src/components/Dialog.tsx
+++ apps/website/src/components/Dialog.tsx
@@ -1,7 +1,37 @@
-export function Dialog() {
+import type { ResolvedChildren } from "solid-js";
+
+export function Dialog({
+  button,
+  children,
+}: {
+  button: ResolvedChildren;
+  children: ResolvedChildren;
+}) {
+  let dialog: HTMLDialogElement | undefined;
+
   return (
-    <dialog>
-      <slot />
-    </dialog>
+    <>
+      <button
+        onClick={() => {
+          if (dialog) {
+            dialog.showModal();
+          }
+        }}
+      >
+        {button}
+      </button>
+      <dialog ref={dialog}>
+        <button
+          onClick={() => {
+            if (dialog) {
+              dialog.close();
+            }
+          }}
+        >
+          close
+        </button>
+        {children}
+      </dialog>
+    </>
   );
 }
