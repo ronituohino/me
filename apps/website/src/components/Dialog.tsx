4e48b91ab423f35d200b46d8506dff4a10668558
Roni Tuohino <rt@ronituohino.com>
2025-04-29 18:12:35 +0300

    Fix styles

--- apps/website/src/components/Dialog.tsx
+++ apps/website/src/components/Dialog.tsx
@@ -1,5 +1,7 @@
 import type { ResolvedChildren } from "solid-js";
 
+import styles from "./Dialog.module.css";
+
 export function Dialog({
   button,
   children,
@@ -13,20 +15,43 @@ export function Dialog({
     <>
       <button
         onClick={() => {
-          if (dialog) {
-            dialog.showModal();
+          if (!dialog) {
+            return;
           }
+
+          dialog.showModal();
         }}
       >
         {button}
       </button>
-      <dialog ref={dialog}>
+      <dialog
+        ref={dialog}
+        onClick={(e) => {
+          // Close dialog when clicking outside of it
+          if (!dialog) {
+            return;
+          }
+
+          const rect = dialog.getBoundingClientRect();
+          const isInDialog =
+            rect.top <= e.clientY &&
+            e.clientY <= rect.top + rect.height &&
+            rect.left <= e.clientX &&
+            e.clientX <= rect.left + rect.width;
+
+          if (!isInDialog) {
+            dialog.close();
+          }
+        }}
+        class={styles.dialog}
+      >
         <button
           onClick={() => {
             if (dialog) {
               dialog.close();
             }
           }}
+          class={styles.close}
         >
           close
         </button>
